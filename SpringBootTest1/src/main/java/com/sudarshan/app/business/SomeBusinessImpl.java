32d20b6885107d950e370595ab5179b20d9f5a74
Sudarshan Pradhan <81807230+sudarshan-pradhan@users.noreply.github.com>
2022-06-17 23:19:30 +0530

    testing using stubs

--- SpringBootTest1/src/main/java/com/sudarshan/app/business/SomeBusinessImpl.java
+++ SpringBootTest1/src/main/java/com/sudarshan/app/business/SomeBusinessImpl.java
@@ -1,11 +1,25 @@
 package com.sudarshan.app.business;
 
+import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.sudarshan.app.data.SomeDataService;
+
 public class SomeBusinessImpl {
+
+    private SomeDataService someDataService;
+
+    public void setSomeDataService(SomeDataService someDataService) {
+		this.someDataService = someDataService;
+	}
     
     public Integer calculateSum(Integer[] data) {
         return Stream.of(data).collect(Collectors.summingInt(x->x));
     }
+
+    public int calculateSumUsingDataService() {
+		int[] data = someDataService.retrieveAllData();
+		return Arrays.stream(data).reduce(Integer::sum).orElse(0);
+	}
 }
---
+++ SpringBootTest1/src/main/java/com/sudarshan/app/data/SomeDataService.java
@@ -0,0 +1,7 @@
+package com.sudarshan.app.data;
+
+public interface SomeDataService {
+
+    int[] retrieveAllData();
+    
+}
