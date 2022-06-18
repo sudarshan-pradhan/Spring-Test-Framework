8bdfa48d6c390a640b91c1854354f792dd6c2142
Sudarshan Pradhan <81807230+sudarshan-pradhan@users.noreply.github.com>
2022-06-18 20:10:41 +0530

    Understanding JSONAssert

---
+++ SpringTestFramework/src/test/java/com/sudarshan/app/spike/JsonAssertTest.java
@@ -0,0 +1,39 @@
+package com.sudarshan.app.spike;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+public class JsonAssertTest {
+    
+    /**
+     * content().json() utilizes JSONAssert,
+     * So this class helps understanding working of JSONAssert
+     */
+    
+    String actualResponse = "{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
+	
+	@Test
+	public void jsonAssert_StrictTrue_ExactMatchExceptForSpaces() throws JSONException {
+		String expectedResponse = "{\"id\": 1, \"name\":\"Ball\", \"price\":10, \"quantity\":100}";
+		JSONAssert.assertEquals(expectedResponse, actualResponse, true);
+	}
+
+	@Test
+	public void jsonAssert_StrictFalse() throws JSONException {
+		String expectedResponse = "{\"id\": 1, \"name\":\"Ball\", \"price\":10}";
+		JSONAssert.assertEquals(expectedResponse, actualResponse, false);
+	}
+
+	@Test
+	public void jsonAssert_WithoutEscapeCharacters() throws JSONException {
+		String expectedResponse = "{id:1, name:Ball, price:10}";
+		JSONAssert.assertEquals(expectedResponse, actualResponse, false);
+	}
+
+    /**
+     * For strict true, we cannot remove any Json element or any escape characters.
+     * But possible with strict false.
+     */
+    
+}
