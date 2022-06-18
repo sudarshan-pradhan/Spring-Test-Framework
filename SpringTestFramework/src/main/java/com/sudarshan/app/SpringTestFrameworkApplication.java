8d648e7386d24e986f1806f0475d5258523f1c7b
Sudarshan Pradhan <81807230+sudarshan-pradhan@users.noreply.github.com>
2022-06-18 18:04:54 +0530

    initial Commit for Spring Boot app for testing

---
+++ SpringTestFramework/src/main/java/com/sudarshan/app/SpringTestFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.sudarshan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestFrameworkApplication.class, args);
+	}
+
+}
