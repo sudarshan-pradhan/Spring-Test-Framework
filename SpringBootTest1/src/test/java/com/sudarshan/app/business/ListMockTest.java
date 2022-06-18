15155b8991abb3706599df63a907f90cb3ada850
Sudarshan Pradhan <81807230+sudarshan-pradhan@users.noreply.github.com>
2022-06-18 15:19:06 +0530

    learn mock and spy

--- SpringBootTest1/src/test/java/com/sudarshan/app/business/ListMockTest.java
+++ SpringBootTest1/src/test/java/com/sudarshan/app/business/ListMockTest.java
@@ -5,11 +5,14 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -17,7 +20,7 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 public class ListMockTest {
-    
+
 	List<String> mock = Mockito.mock(List.class);
 
 	@Test
@@ -65,35 +68,76 @@ public class ListMockTest {
 
 	@Test
 	public void argumentCapturing() {
-		
-		//SUT
+
+		// SUT
 		mock.add("SomeString");
-		
-		//Verification
+
+		// Verification
 		ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
 		verify(mock).add(captor.capture());
-		
+
 		assertEquals("SomeString", captor.getValue());
-		
+
 	}
-	
+
 	@Test
 	public void multipleArgumentCapturing() {
-		
-		//SUT
+
+		// SUT
 		mock.add("SomeString1");
 		mock.add("SomeString2");
-		
-		//Verification
+
+		// Verification
 		ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-		
+
 		verify(mock, times(2)).add(captor.capture());
-		
+
 		List<String> allValues = captor.getAllValues();
-		
+
 		assertEquals("SomeString1", allValues.get(0));
 		assertEquals("SomeString2", allValues.get(1));
-		
+
+	}
+
+	/**
+	 * Mocking basically omits the call / code that we want to mock and gives use a
+	 * return value
+	 * without the code execution.
+	 */
+	@Test
+	public void mocking() {
+		ArrayList arrayListMock = mock(ArrayList.class);
+		System.out.println(arrayListMock.get(0));// null
+		System.out.println(arrayListMock.size());// 0
+		arrayListMock.add("Test");
+		arrayListMock.add("Test2");
+		System.out.println(arrayListMock.size());// 0
+		when(arrayListMock.size()).thenReturn(5);
+		System.out.println(arrayListMock.size());// 5
+	}
+
+	/**
+	 * spying bascically is opposite to mock, it actually executed the peice of code
+	 * and
+	 * then returns response according to actual value and operation.
+	 */
+	@Test
+	public void spying() {
+		ArrayList arrayListSpy = spy(ArrayList.class);
+		arrayListSpy.add("Test0");
+		System.out.println(arrayListSpy.get(0));// Test0
+		System.out.println(arrayListSpy.size());// 1
+		arrayListSpy.add("Test");
+		arrayListSpy.add("Test2");
+		System.out.println(arrayListSpy.size());// 3
+
+		when(arrayListSpy.size()).thenReturn(5);
+		System.out.println(arrayListSpy.size());// 5
+
+		arrayListSpy.add("Test4");
+		System.out.println(arrayListSpy.size());// 5
+
+		verify(arrayListSpy).add("Test4");
 	}
 
 }
