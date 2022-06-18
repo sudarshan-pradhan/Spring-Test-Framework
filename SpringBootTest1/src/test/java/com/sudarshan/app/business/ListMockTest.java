5d1697ee4534c42f6babddfe61487d4a88936b87
Sudarshan Pradhan <81807230+sudarshan-pradhan@users.noreply.github.com>
2022-06-18 06:42:37 +0530

    Learning Mock operations using java.util.List class to test

---
+++ SpringBootTest1/src/test/java/com/sudarshan/app/business/ListMockTest.java
@@ -0,0 +1,37 @@
+package com.sudarshan.app.business;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class ListMockTest {
+    
+	List<String> mock = Mockito.mock(List.class);
+
+	@Test
+	public void size_basic() {
+		when(mock.size()).thenReturn(5);
+		assertEquals(5, mock.size());
+	}
+
+	@Test
+	public void returnDifferentValues() {
+		when(mock.size()).thenReturn(5).thenReturn(10);
+		assertEquals(5, mock.size());
+		assertEquals(10, mock.size());
+	}
+
+	@Test
+	@Disabled
+	public void returnWithParameters() {
+		when(mock.get(0)).thenReturn("Sudarshan");
+		assertEquals("Sudarshan", mock.get(0));
+		assertEquals(null, mock.get(1));
+	}
+
+}
